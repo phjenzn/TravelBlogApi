93cb15c7685a03386969e15e11449f8699b33be6
Christian Bamatembera <chrisbama92@gmail.com>
2017-04-07 22:24:30 -0400

    First commit
    Setup of cloudinary and mongodb

---
+++ src/main/java/com/palyfight/TravelBlogApi/App.java
@@ -0,0 +1,9 @@
+package com.palyfight.TravelBlogApi;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/palyfight/TravelBlogApi/DatabaseService.java
@@ -0,0 +1,36 @@
+package com.palyfight.TravelBlogApi;
+
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseService {
+	private MongoClient client;
+	private MongoDatabase db;
+	
+	DatabaseService(){
+		if(this.db == null){
+			String dbUser = System.getenv("DB_USER");
+			String dbPassword = System.getenv("DB_PASSWORD");
+			String dbHost = System.getenv("DB_HOST");
+			int dbPort = Integer.parseInt(System.getenv("DB_PORT"));
+			String dbName = "travelblogdb";
+			
+			String connStr = String.format("mongodb://%s:%s@%s:%d/%s", dbUser, dbPassword, dbHost, dbPort, dbName);
+			System.out.println("YOLO => " + connStr);
+			MongoClientURI uri  = new MongoClientURI(connStr); 
+	        this.client = new MongoClient(uri);
+	        this.db = client.getDatabase(uri.getDatabase());
+		}
+	}
+	
+	public void save(String url, String category){
+		MongoCollection<Document> images = db.getCollection("PalyTravelCollection");
+		Document image = new Document("url", url).append("category", category);
+		images.insertOne(image);
+		
+		client.close();
+	}
+}
